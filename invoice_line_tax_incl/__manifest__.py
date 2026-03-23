a04db60547d5a38e8ae37246081b74c052ac6be9
Hammerov <619hamzalk@gmail.com>
2026-03-23 11:05:35 +0300

    manifest.py added

---
+++ invoice_line_tax_incl/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Invoice Line Tax Inclusive Column",
+    "version": "1.0",
+    "summary": "Adds Total (Incl. Tax) column to invoice lines",
+    "author": "You",
+    "category": "Accounting",
+    "depends": ["account"],
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}

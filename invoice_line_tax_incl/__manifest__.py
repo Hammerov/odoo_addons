af3151e06de77083def0c9390e7bec74c30f007d
Hammerov <619hamzalk@gmail.com>
2026-03-23 14:53:26 +0300

    modified invoice printout to include column

--- invoice_line_tax_incl/__manifest__.py
+++ invoice_line_tax_incl/__manifest__.py
@@ -1,13 +1,11 @@
 {
     "name": "Invoice Line Tax Inclusive Column",
     "version": "1.0",
-    "summary": "Adds Total (Incl. Tax) column to invoice lines",
-    "author": "You",
     "category": "Accounting",
     "depends": ["account"],
     "data": [
         "views/account_move_views.xml",
+        "views/report_invoice_templates.xml", # Add this line
     ],
     "installable": True,
-    "application": False,
 }
